584e2de2fb700e3ddeea500112d43e0a82e25ad0
rhegishte27 <35556401+rhegishte27@users.noreply.github.com>
2021-03-16 14:04:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   version = "~> 3.0"
   region  = "ap-south-1"
 }
+
+resource "aws_cloudwatch_log_group" "common-loggroup" {
+  name = "common_loggroup-rupesh"
+
+  tags = {
+    Environment = "production"
+    Application = "serviceA"
+  }
+}
