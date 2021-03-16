427516b42b2193577f9f9e7f69a2e6759e8b1012
rhegishte27 <35556401+rhegishte27@users.noreply.github.com>
2021-03-16 14:10:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version = "~> 3.0"
   region  = "ap-south-1"
-  access_key = "AKIAYIJETJ6IFXUFIFJU"
-  secret_key = "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"  
+  access_key = ""
+  secret_key = ""  
 }
 
 resource "aws_cloudwatch_log_group" "common-loggroup" {
